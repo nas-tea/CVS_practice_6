7fd8630f97bdbf97d91eb16e75428db650cf4782
Mr. Otval <72942698+MasterVasily@users.noreply.github.com>
2024-02-07 15:59:01 +0500

    Update test.py

--- test.py
+++ test.py
@@ -14,3 +14,12 @@ for i in range(N-1):
             a[j], a[j+1] = a[j+1], a[j]
  
 print(a)
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
